bb0ab89f5ce208e287e03e78a5da020f0eb792a6
sanjanadondapati <99525594+sanjanadmd@users.noreply.github.com>
2022-05-21 10:39:16 +0530

    Modified parseArgs contract
    * Added setDelimiter and validateOption

--- src/parseArgs.js
+++ src/parseArgs.js
@@ -1,13 +1,39 @@
+const setDelimiter = (key, defaultOptions) => {
+  if (defaultOptions[key] !== undefined) {
+    return defaultOptions[key];
+  }
+  return '\n';
+};
+
+const validateOption = (defaultOptions, delimiter, option) => {
+  const error = {};
+  if (defaultOptions[option] === undefined) {
+    error.name = 'illegal Option';
+    error.message = 'option not found';
+  }
+  if (delimiter !== defaultOptions[option]) {
+    error.name = 'SyntaxError';
+    error.message = 'can not combine line and byte counts';
+  }
+  return error;
+};
+
 const parseArgs = (args) => {
   const defaultOptions = { '-n': '\n', '-c': '' };
   const options = { lines: 10, delimiter: '\n' };
   const fileNames = [];
+  options.delimiter = setDelimiter(args[0], defaultOptions);
   for (let index = 0; index < args.length; index += 2) {
     if (args[index].match(/^[^-]/)) {
       fileNames.push(...args.slice(index));
       return { fileNames, options };
     }
-    options.delimiter = defaultOptions[args[index]];
+    const error = validateOption(
+      defaultOptions, options.delimiter, args[index]
+    );
+    if (Object.keys(error).length > 0) {
+      return { error };
+    }
     options.lines = +args[index + 1];
   }
 
@@ -15,3 +41,5 @@ const parseArgs = (args) => {
 };
 
 exports.parseArgs = parseArgs;
+exports.setDelimiter = setDelimiter;
+exports.validateOption = validateOption;
--- test/testParseArgs.js
+++ test/testParseArgs.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const { parseArgs } = require('../src/parseArgs');
+const { parseArgs, setDelimiter, validateOption } = require('../src/parseArgs');
 
 describe('parseArgs', () => {
   it('should provide all fileNames and default options', () => {
@@ -44,9 +44,48 @@ describe('parseArgs', () => {
   it('should not provide fileNames starting with hyphen', () => {
     assert.deepStrictEqual(parseArgs(['-hello.txt']),
       {
-        fileNames: [],
-        options: { lines: NaN, delimiter: undefined }
+        error: {
+          name: 'SyntaxError',
+          message: 'can not combine line and byte counts'
+        }
       });
   });
 
+  it('should not change option once it is fixed in the start', () => {
+    assert.deepStrictEqual(parseArgs(['-n', '4', '-c', 1, 'a.txt']),
+      {
+        error: {
+          name: 'SyntaxError',
+          message: 'can not combine line and byte counts'
+        }
+      });
+  });
+
+});
+
+describe('setDelimiter', () => {
+  it('should return delimiter if the option is found', () => {
+    assert.strictEqual(setDelimiter('-n', { '-n': '\n' }), '\n');
+    assert.strictEqual(setDelimiter('-c', { '-c': '' }), '');
+  });
+  it('should return "\n" as delimiter when option not found', () => {
+    assert.strictEqual(setDelimiter('hello', {}), '\n');
+    assert.strictEqual(setDelimiter('-a', {}), '\n');
+  });
+});
+
+describe('validateOptions', () => {
+  it('should return an error when option not found', () => {
+    assert.deepStrictEqual(validateOption({}, '\n', '-a'), {
+      name: 'SyntaxError',
+      message: 'can not combine line and byte counts'
+    });
+  });
+
+  it('should return an error when option won\'t match with given value', () => {
+    assert.deepStrictEqual(validateOption({}, '\n', '-a'), {
+      name: 'SyntaxError',
+      message: 'can not combine line and byte counts'
+    });
+  });
 });
